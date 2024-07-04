cbc65cbff210f540133da730890b6770b9a8057f
MohamedAshiquek <asiashique9747@gmail.com>
2024-07-04 20:31:01 +0530

    first commit

---
+++ src/main/java/com/innoit/todoapplication/api/ToDoApplication.java
@@ -0,0 +1,12 @@
+package com.innoit.todoapplication.api;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+	}
+
+}
