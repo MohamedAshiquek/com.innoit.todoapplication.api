8ee92f6bf50be7489304560a3396aad55ca4ddfe
MohamedAshiquek <asiashique9747@gmail.com>
2024-07-04 20:46:12 +0530

    change running port number

--- src/main/java/com/innoit/todoapplication/api/ToDoApplication.java
+++ src/main/java/com/innoit/todoapplication/api/ToDoApplication.java
@@ -1,12 +1,19 @@
 package com.innoit.todoapplication.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class ToDoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoApplication.class, args);
+	}
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizer() {
+		return factory -> factory.setPort(8081);
 	}
 
 }
